1547c13c6f72d324cf79f461cf60727e01a0a483
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-07-10 12:26:00 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { render } from 'react-dom';
+
+const App = () => (
+  <div className="container">
+    <h1 className="text-lg">Rick & Morty App</h1>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
