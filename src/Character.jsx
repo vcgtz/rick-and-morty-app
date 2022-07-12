bd9728861b3da8f1368266f85a84a3c7eb18b127
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-07-11 20:50:06 -0500

    Set basic stye for the character component

--- src/Character.jsx
+++ src/Character.jsx
@@ -1,20 +1,19 @@
 const Character = () => {
   return (
-    <div className="rounded-md border-2 flex">
+    <div className="rounded-md border-2 flex items-stretch">
       <img
-        className="w-48 h-48 rounded-l-md"
+        className="w-36 h-36 rounded-l-md"
         src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
         alt="Rick Sanchez" />
        <div className="p-2 w-full">
-        <div>
-          <p>Rick Sanchez</p>
-          <p>Alive</p>
-          <p>Human</p>
-          <p>Male</p>
-        </div>
-        <div className="grid grid-cols-2">
-          <div>Origin: <a href="#">Earth (C-137)</a></div>
-          <div>Location: <a href="#">Citadel of Ricks</a></div>
+        <div className="grid grid-cols-1">
+          <p className="text-lg font-bold">
+            <span className="text-gray-300 font-normal">#1</span> Rick Sanchez
+          </p>
+          <p>🟢 Alive</p>
+          <p>🚹 Male</p>
+          <p>Human from <a className="text-orange-400" href="#">Earth (C-137)</a></p>
+          <p>Last Location: <a className="text-orange-400" href="#">Citadel of Ricks</a></p>
         </div>
        </div>
     </div>
