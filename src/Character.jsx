83012738cc0aad07ec2fc9079930b9f712415404
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-07-11 21:24:06 -0500

    Show the list of the first 20 characters

--- src/Character.jsx
+++ src/Character.jsx
@@ -1,19 +1,42 @@
-const Character = () => {
+const Character = (props) => {
+  const { id, name, status, species, gender, origin, location, image } = props.info;
+
+  const getGenderIcon = () => {
+    const genderList = {
+      'Male': '🚹',
+      'Female': '🚺',
+      'Genderless': '☮️',
+      'unknown': '❓'
+    };
+
+    return genderList[gender];
+  };
+
+  const getStatusIcon = () => {
+    const statusList = {
+      'Alive': '🟢',
+      'Dead': '🔴',
+      'unknown': '🟤'
+    }
+
+    return statusList[status];
+  }
+
   return (
-    <div className="rounded-md border-2 flex items-stretch">
+    <div className="rounded-md border-2 flex items-stretch my-2">
       <img
         className="w-36 h-36 rounded-l-md"
-        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-        alt="Rick Sanchez" />
+        src={image}
+        alt={name} />
        <div className="p-2 w-full">
         <div className="grid grid-cols-1">
           <p className="text-lg font-bold">
-            <span className="text-gray-300 font-normal">#1</span> Rick Sanchez
+            <span className="text-gray-300 font-normal">#{id}</span> {name}
           </p>
-          <p>🟢 Alive</p>
-          <p>🚹 Male</p>
-          <p>Human from <a className="text-orange-400" href="#">Earth (C-137)</a></p>
-          <p>Last Location: <a className="text-orange-400" href="#">Citadel of Ricks</a></p>
+          <p>{getStatusIcon()} {status}</p>
+          <p>{getGenderIcon()} {gender}</p>
+          <p>{species} from <a className="text-orange-400" href="#">{origin.name}</a></p>
+          <p>Last Location: <a className="text-orange-400" href="#">{location.name}</a></p>
         </div>
        </div>
     </div>
--- src/CharacterList.jsx
+++ src/CharacterList.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import Character from "./Character.jsx";
 
 const CharacterList = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    requestCharacters();
+  }, []);
+
+  const requestCharacters = async () => {
+    const res = await fetch('https://rickandmortyapi.com/api/character');
+    const json = await res.json();
+
+    setCharacters(json.results);
+  };
+
   return (
     <div className="w-1/2 mx-auto">
-      <Character />
+      {
+        characters.map((character) => (
+          <Character
+            info={character}
+            key={character.id} />
+        ))
+      }
     </div>
   );
 };
